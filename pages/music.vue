948d65653a64a608249d271abf28c08c6f74a212
Guilherme Pangnotta <setetres@gmail.com>
2020-05-23 13:54:56 -0300

    Music

--- pages/music.vue
+++ pages/music.vue
@@ -4,6 +4,37 @@
             <div class="ooo-container _p-0">
                 <div class="ooo-row">
                     <div class="ooo-col-md-3">
+                        <img src="/images/triceracough.png" class="_w-100" height="auto">
+                    </div>
+                    <div class="ooo-col-md-9">
+                        <p class="section__title _mb-0">
+                            TRICERACOUGH
+                        </p>
+                        <p class="small">2020 — by XXXXXXXOOO</p>
+                        <hr>
+                        <div class="ooo-row">
+                            <div class="ooo-col-12">
+                                <p class="small _mb-0">1.</p>
+                            </div>
+                            <div class="ooo-col-8">
+                                TRICERACOUGH
+                            </div>
+                            <div class="ooo-col-4">
+                                <p class="small _mb-0 _text-right">3:34</p>
+                            </div>
+                        </div>
+                        <hr>
+                        <p>
+                            <a href="https://xxxxxxx.ooo" class="link link--external link--button _mr-4" target="_blank" rel="noopener">Listen now</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="gutter">
+            <div class="ooo-container _p-0">
+                <div class="ooo-row">
+                    <div class="ooo-col-md-3">
                         <img src="/images/fein.png" class="_w-100" height="auto">
                     </div>
                     <div class="ooo-col-md-9">
@@ -57,37 +88,6 @@
                             </div>
                             <div class="ooo-col-4">
                                 <p class="small _mb-0 _text-right">2:45</p>
-                            </div>
-                        </div>
-                        <hr>
-                        <p>
-                            <a href="https://xxxxxxx.ooo" class="link link--external link--button _mr-4" target="_blank" rel="noopener">Listen now</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="gutter">
-            <div class="ooo-container _p-0">
-                <div class="ooo-row">
-                    <div class="ooo-col-md-3">
-                        <img src="/images/triceracough.png" class="_w-100" height="auto">
-                    </div>
-                    <div class="ooo-col-md-9">
-                        <p class="section__title _mb-0">
-                            TRICERACOUGH
-                        </p>
-                        <p class="small">2020 — by XXXXXXXOOO</p>
-                        <hr>
-                        <div class="ooo-row">
-                            <div class="ooo-col-12">
-                                <p class="small _mb-0">1.</p>
-                            </div>
-                            <div class="ooo-col-8">
-                                TRICERACOUGH
-                            </div>
-                            <div class="ooo-col-4">
-                                <p class="small _mb-0 _text-right">3:34</p>
                             </div>
                         </div>
                         <hr>
